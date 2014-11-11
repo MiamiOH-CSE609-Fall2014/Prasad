1a41ae403375505c73f2e9b2abb0141546522700
prasadas <prasadas@muohio.edu>
2014-11-11 18:45:57 -0500

    Part 2 cont1

--- project3/dna.cpp
+++ project3/dna.cpp
@@ -5,16 +5,17 @@
 #include<vector>
 #include<tuple>
 #include<sstream>
+#include<algorithm>
 using namespace std;
-string comb[]= {"AA","AG","AC","AT","GA","GG","GC","GT","CA","CG","CC","CT","TA","TG","TC","TT"}; 
+string  comb[]= {"AA","AG","AC","AT","GA","GG","GC","GT","CA","CG","CC","CT","TA","TG","TC","TT"};
+vector<string> co (comb, comb + sizeof(comb) / sizeof(string) ); 
 tuple<vector<string>,vector<string>,string > parseFastfile(string s)
 {
   ifstream file(s);
   string f1; 
   vector <string> h;
   vector <string> c;
-  string se = "";
-    
+  string se = "";  
   if (file.is_open())
     {   
   while (getline(file,f1))
@@ -30,27 +31,47 @@ tuple<vector<string>,vector<string>,string > parseFastfile(string s)
 	  else 
             {   
 	      se = se + f1;
-            }
+              }
 	    
 
 	}
-
- 
+  
       return make_tuple(h,c,se);
 		  	  
     }
 
 }
 
+
 map <string , int > digramFreqScores(string si)
 {
-  map <string , int > m;
+  int c;
+ vector<int> n;
+string ::size_type i = 0;
+for (int j =0; j < co.size(); j++)
+   {
+     i = 0; 
+     c = 0;   
+while ((i = si.find(co[j],i)) != string::npos) 
+{
+   ++c;
+   if ((j == 0) or (j == 5) or (j == 10) or (j == 15))
+     {
+   i = i + 1;
+     } 
+   else 
+     {
+       i = i + 2;
+     }
+ }
+   
+ n.push_back(c);
+}
 
-  for (int i =0; i < si.size() ;i++)
-    {
-      
-    }
-
+ for (int i =0; i< n.size();i++)
+   {
+     cout << n[i] << endl;
+   } 
 }
 
 vector< vector<int> > digramFreqMatrix( map< string, int > M1)
@@ -61,21 +82,13 @@ vector< vector<int> > digramFreqMatrix( map< string, int > M1)
 int main()
 {
   string filepath;
-   
   cout << "Enter the Fasta file to be parsed " <<endl;
   cin  >> filepath;
   tuple<vector<string>,vector<string>,string> t1;
   t1 =  parseFastfile(filepath);
-  cout << get<2> (t1)<< endl;
-  /*  for ( int i =0; i< get<0>(t1).size();i++)
-  {
-    cout << get<0>(t1)[i] << endl; 
-    
-    }*/
-
   map <string , int > scores = digramFreqScores(get<2> (t1));
-
-
+ 
+  
     
 
   
