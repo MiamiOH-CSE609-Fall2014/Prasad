f4871629a67d9b965f63f3d0b548b9fed1dc4e5d
prasadas <prasadas@muohio.edu>
2014-11-13 13:16:49 -0500

    Part 3.1

--- project3/dna.cpp
+++ project3/dna.cpp
@@ -102,24 +102,75 @@ vector< vector<int> > digramFreqMatrix( map<string,int>  scores)
   return c;     
 }
 
+
+vector< vector<int> >  parseScoringFile(string line)
+{
+  vector< vector <int> > l;
+  ifstream file1(line);
+  string f;
+  string f1;
+  string f2;
+  string f3;
+  string f4;
+  vector<int> g1;
+
+  if (file1.is_open())
+    {
+      while (getline(file1,f))
+     {
+
+ stringstream i(f);
+ getline(i,f1,',') ;
+ g1.push_back(atoi(f1.c_str()));
+ getline(i,f2,',') ;
+ g1.push_back(atoi(f2.c_str()));
+ getline(i,f3,',') ;
+ g1.push_back(atoi(f3.c_str()));
+ getline(i,f4,',') ;
+ g1.push_back(atoi(f4.c_str()));
+ l.push_back(g1);
+ g1.clear();
+ 
+
+    }
+    }
+  /*     for (int n1 = 0 ; n1 < l.size() ;n1++)
+    {
+      for (int n3=0 ; n3 < l[n1].size()  ; n3++ )
+      {
+      cout << l[n1][n3] << "\t" ;
+      }
+      cout << "\n";
+      } */
+
+  return l;
+
+}
+
+
 int main()
 {
   string filepath;
+  string scorepath;
   cout << "Enter the Fasta file to be parsed " <<endl;
   cin  >> filepath;
   tuple<vector<string>,vector<string>,string> t1;
   t1 =  parseFastfile(filepath);
   map <string , int > scores = digramFreqScores(get<2> (t1));
   vector< vector <int> > m5 =  digramFreqMatrix(scores);
+  cout << " Enter the score CSV file  " << endl;
+  cin >> scorepath ;
+  vector< vector <int> > m6 = parseScoringFile(scorepath);
+
  
-     for (int n1 = 0 ; n1 < m5.size() ;n1++)
+  /*  for (int n1 = 0 ; n1 < m5.size() ;n1++)
     {
       for (int n3=0 ; n3 < m5[n1].size()  ; n3++ )
       {
       cout << m5[n1][n3] << "\t" ;
       }
       cout << "\n";
-      } 
+      } */
   
   
 
